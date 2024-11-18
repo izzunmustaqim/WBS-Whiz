c8f9ca7118217e0e8d04e3326f00ab3fd07c8b72
Jannatil Anwar Bin Jaharuddin <jannatilanwar.jaharuddin@fujitsu.com>
2024-11-18 07:54:51 +0000

    Updated main.py latest by Anwar 11/18/2024

--- main/main.py
+++ main/main.py
@@ -7,6 +7,7 @@ import pandas as pd  # Import pandas
 import config   # Import the config file
 import webbrowser
 from tkcalendar import DateEntry
+import re
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -14,26 +15,31 @@ class Application(tk.Frame):
         self.master = master
         self.grid()
         self.skillset_file = None  # Initialize skillset_file
-        self.task_details_file = None  # Initialize task_file (Mira change the name to self.task_details_file - 5/11)
+        self.task_details_file = None  # Initialize task_file
         self.create_widgets()
         self.skill_set_data = None  # Initialize skill_set_data
 
     def create_widgets(self):
+        # Add a new entry for the API key
+        tk.Label(self, text="API Key").grid(row=0, column=0, padx=10, pady=5, sticky='w')
+        self.api_key_entry = tk.Entry(self, width=40, show='*')
+        self.api_key_entry.grid(row=0, column=1, padx=10, pady=5, sticky='we')
+
         labels = ["Members skill set", "Task details"]
         descriptions = [
             ("Download template here: Members_skillset.xlsx", "https://fujitsu.sharepoint.com/:x:/r/teams/Asia-42f6e454-ChatAIContestAPG/Shared%20Documents/ChatAI%20Contest%20APG/Deliverable/Sprint%201/MEMBERS_SKILLSET.xlsx?d=w3087fb3ba54e43bab309789ad185a9a7&csf=1&web=1&e=5ekEi5"),
-            ("Download template here: Task_details.xlsx", "https://fujitsu.sharepoint.com/:x:/r/teams/Asia-42f6e454-ChatAIContestAPG/Shared%20Documents/ChatAI%20Contest%20APG/Deliverable/Sprint%201/Task%20Details%20Sample.xlsx?d=we84ab6e37355440c9f70aa90c9f85bdc&csf=1&web=1&e=FtsjuW")
+            ("Download template here: Task_details.xlsx", "https://fujitsu.sharepoint.com/:x:/r/teams/Asia-42f6e454-ChatAIContestAPG/Shared%20Documents/ChatAI%20Contest%20APG/Deliverable/Sprint%202/Task%20Details%20Sample.xlsx?d=w4ffbd7b446c146539859793651360c36&csf=1&web=1&e=jUauGt")
         ]
         self.entries = []
         for i, (label, (description, url)) in enumerate(zip(labels, descriptions)):
-            tk.Label(self, text=label).grid(row=i*2, column=0, padx=10, pady=5, sticky='w')
+            tk.Label(self, text=label).grid(row=(i+1)*2, column=0, padx=10, pady=5, sticky='w')
             entry = tk.Entry(self, width=40)
-            entry.grid(row=i*2, column=1, padx=10, pady=5)
+            entry.grid(row=(i+1)*2, column=1, padx=10, pady=5)
             self.entries.append(entry)
-            tk.Button(self, text="Browse", command=lambda e=entry, l=label: self.browse_file(e, l), width=10).grid(row=i*2, column=2, padx=10, pady=5)
+            tk.Button(self, text="Browse", command=lambda e=entry, l=label: self.browse_file(e, l), width=10).grid(row=(i+1)*2, column=2, padx=10, pady=5)
             
             text_widget = tk.Text(self, height=1, width=40, font=("Helvetica", 8), bd=0, bg=self.cget("bg"))
-            text_widget.grid(row=i*2+1, column=1, padx=10, sticky='w')
+            text_widget.grid(row=(i+1)*2+1, column=1, padx=10, sticky='w')
             text_widget.insert(tk.END, description)
             
             if "Members_skillset.xlsx" in description:
@@ -76,22 +82,14 @@ class Application(tk.Frame):
         webbrowser.open_new(url)
     
     def main(self):
+        self.api_key = self.validate_api_key(self.api_key_entry.get())
+        if not self.api_key:
+            return
+
         # Call read file function
         self.skill_set_data = self.read_file(self.skillset_file, 3, None)
         self.task_details_data = self.read_file(self.task_details_file, 1, "B:E")
         self.send_data_to_chatai()
-
-
-        #Test print from config file
-        print(f"Test chatAI Key : {config.ChatAI_key}")
-
-        # Use the prompt_message_template from config and format it with the data
-        prompt_message = config.prompt_message_template.format(
-            task_details_data=self.task_details_data.to_json(),
-            skill_set_data=self.skill_set_data.to_json()
-        )
-
-        self.read_api_key()
 
     # rangecol=None mean will read all columns that has data
     def read_file(self, file, rowskip=0, rangecol=None):
@@ -117,6 +115,7 @@ class Application(tk.Frame):
     #Send data to ChatAI for analysis
     def send_data_to_chatai(self):
         try:
+
             self.status_label.config(text="Processing, please wait...")
             self.status_label.update_idletasks()  # Force the GUI to update
 
@@ -133,34 +132,24 @@ class Application(tk.Frame):
             print(end_date_str)
             print(start_date)
             print(end_date)
-            
 
             # Define the API endpoint and hardcoded prompt
             api_endpoint = "https://ai-foundation-api.app/ai-foundation/chat-ai/gpt4"
-            prompt = (
-                "#Input"
-                f"{self.task_details_data.to_json()} "
-                f"{self.skill_set_data.to_json()}"
-
-                "#INSTRUCTIONS"
-                "As a project manager, create a WBS for the project. The input are the task details and the skill set of the team members."
-                "Please give in table format where each of the title should be the header"
-                "For high task priority assign first to senior developer then to middle developer"
-                "For medium task priority assign first to middle developer then junior developer" 
-                "For low task priority first assign to junior developer then middle developer"
-                "Please also consider the skills needed for each task with the member with high level skills for that particular need"
-                "The output should include all members for each task and one task for one person only"
-                "Please estimate the duration of each task without including it in the result. Then, set the start date and end date based on the duration."
-                f"Please use the date range from the input data for start date = {start_date_str} and end date = {end_date_str} for each of the task."
-                f"Please fully utilize the date but do not exceed the project's end date at {end_date_str}"
-                "It should consists of:"
-                
-                "| Item No. {n} | Task Description: {Task Description} | Assigned to: {Assigned to based on the input data of the task} | Progress: {Progress of the task either To do/In progress/Waiting for Review/Done, default is To do} | Plan Start date: {Start date based on the input data of the task} | Plan End date: {End date based on the input data of the task} |\n"
-            )
+            prompt = config.prompt.format(
+                            task_details_data=self.task_details_data.to_json(),
+                            skill_set_data=self.skill_set_data.to_json(),
+                            start_date_str=start_date_str,
+                            end_date_str=end_date_str,
+                            task_description="Task Description Example",  # Provide example values for placeholders
+                            assigned_to="Assigned to Example",
+                            progress="To do",
+                            plan_start_date="Start date Example",
+                            plan_end_date="End date Example"
+                        )
 
             headers = {
                 "Content-type": "application/json",
-                "api-key": "OnGCTtBUL4T2TrL6osaPi6hFInUhGzMAE3BmAp7loukbAiHE"
+                "api-key": self.api_key
             }
 
             payload = {
@@ -200,16 +189,19 @@ class Application(tk.Frame):
         finally:
             self.status_label.config(text="Process completed")
 
-    def read_api_key(self):
-        try:
-            with open("API_Key.txt",'r') as file:
-                content = file.read()
-                print(content)
-        except FileNotFoundError:
-            messagebox.showerror("Error", config.error_message["APIKeyError"])
-        except Exception as e:
-            error_message = config.error_message["FailReadError"].format(error_message=str(e))
-            messagebox.showerror("Error", error_message)
+    def validate_api_key(self, api_key):
+        pattern = r'^[A-Za-z0-9]{48}$'        
+        if not api_key:
+            messagebox.showerror("Error", config.error_message["APIEmptyField"])
+            return None
+        elif re.search(r'[\uFF01-\uFF60\uFFE0-\uFFE6]', api_key):
+            messagebox.showerror("Error", config.error_message["FullWidthCharacterError"])
+            return None
+        elif re.match(pattern, api_key):
+            return api_key
+        else:
+            messagebox.showerror("Error", config.error_message["InvalidKeyError"])
+            return None
 
 root = tk.Tk()
 root.title("WBS Enhancement")
